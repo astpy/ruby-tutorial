af7e4f852785a1fc7b22d828e67624b09f57e082
astpy <sketdance551@gmail.com>
2022-01-24 18:05:05 +0900

    try removing while(true)

--- app/assets/stylesheets/home.js
+++ app/assets/stylesheets/home.js
@@ -1,3 +1,105 @@
+function search_song_array(){
+
+    var url = "";
+
+    for(var i = 9; i <= 19; i++){
+        url = "/json/" + i + ".json";
+        $.getJSON(url, function(data){
+            diff_arrays = [...Array(data.length).keys()].map((d) => {return d});
+        });
+    }
+
+    console.log(diff_arrays);
+
+    var already = [];
+    var alert_text = "";
+
+    // 1曲目
+    var diff = document.getElementById('1st').value;
+    if(diff < 9 || diff > 19){
+        alert_text += "1曲目 : 9以上19以内で入力してください。\n";
+    }else{
+        var url = "/json/" + diff + ".json";
+        $.getJSON(url, function(data){
+            var song_index = Math.floor(Math.random() * data.length);
+            already.push([diff, song_index]);
+            document.getElementById("pic1").innerText = data[song_index]["曲名"];
+        });
+    }
+
+    // 2曲目
+    var diff = document.getElementById('2nd').value;
+    if(diff < 9 || diff > 19){
+        alert_text += "2曲目 : 9以上19以内で入力してください。\n";
+    }else{
+        var url = "/json/" + diff + ".json";
+
+        $.getJSON(url, function(data){
+            if(document.getElementById("overlap").checked == true){
+                var song_index = Math.floor(Math.random() * data.length);
+            }else{
+                while(true){
+                    var song_index = Math.floor(Math.random() * data.length);
+                    if(overlap_check(diff, song_index, already) == true){
+                        break;
+                    }
+                }
+            }
+            already.push([diff, song_index]);
+            document.getElementById("pic2").innerText = data[song_index]["曲名"];
+        });
+    }
+
+    var diff = document.getElementById('3rd').value;
+    if(diff < 9 || diff > 19){
+        alert_text += "3曲目 : 9以上19以内で入力してください。\n";
+    }else{
+        var url = "/json/" + diff + ".json";
+
+        $.getJSON(url, function(data){
+            if(document.getElementById("overlap").checked == true){
+                var song_index = Math.floor(Math.random() * data.length);
+            }else{
+                while(true){
+                    var song_index = Math.floor(Math.random() * data.length);
+                    if(overlap_check(diff, song_index, already) == true){
+                        break;
+                    }
+                }
+            }
+            already.push([diff, song_index]);
+            document.getElementById("pic3").innerText = data[song_index]["曲名"];
+        });
+    }
+
+    var diff = document.getElementById('ex').value;
+    if(diff < 9 || diff > 19){
+        alert_text += "EXTRA : 9以上19以内で入力してください。";
+    }else{
+        var url = "/json/" + diff + ".json";
+
+        $.getJSON(url, function(data){
+            if(document.getElementById("overlap").checked == true){
+                var song_index = Math.floor(Math.random() * data.length);
+            }else{
+                while(true){
+                    var song_index = Math.floor(Math.random() * data.length);
+                    if(overlap_check(diff, song_index, already) == true){
+                        break;
+                    }
+                }
+            }
+            document.getElementById("pic4").innerText = data[song_index]["曲名"];
+        });
+    }
+
+    if(alert_text != ""){
+        alert(alert_text);
+    }
+
+}
+
+
 function overlap_check(diff, index, already){
 
     for(var i = 0; i < already.length; i++){
@@ -12,8 +114,7 @@ function overlap_check(diff, index, already){
 }
 
 function search_song(){
-
-    var difficulties = [];
+    
     var already = [];
     var alert_text = "";
 
